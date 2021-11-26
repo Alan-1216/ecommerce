348811bfc81cd841ed37eab584d3e8cd90256128
Alan Felipe <alanfelipeop@outlook.com>
2021-11-26 17:45:29 -0300

    Site Produtos

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -15,6 +15,20 @@ class Product extends Model{
 		
 	}
 
+	public static function checkList($list)
+	{
+
+		foreach($list as &$row){
+
+			$p = new Product();
+			$p->setData($row);
+			$row = $p->getValues();
+
+		}
+
+		return $list;
+	}
+
 	public function save()
 	{
 
@@ -64,7 +78,7 @@ class Product extends Model{
 		if(file_exists($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "site" . DIRECTORY_SEPARATOR . "img" . DIRECTORY_SEPARATOR . "products" . DIRECTORY_SEPARATOR . $this->getidproduct() . ".jpg"))
 		{
 
-			$url = "/res/site/img/products" . $this->getidproduct() . ".jpg";
+			$url = "/res/site/img/products/" . $this->getidproduct() . ".jpg";
 
 		} else
 		{
