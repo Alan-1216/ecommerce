20fdffca65a7fb601ece5bbf79e97ba02aded153
Alan Felipe <alanfelipeop@outlook.com>
2021-12-06 19:04:12 -0300

    Administração - Pedidos

--- vendor/hcodebr/php-classes/src/Model/Order.php
+++ vendor/hcodebr/php-classes/src/Model/Order.php
@@ -1,12 +1,16 @@
 <?php
 
-namespace Hcode/Model;
+namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
 use \Hcode\Model;
+use \Hcode\Cart;
 
 Class Order extends Model{
 
+	const SUCCESS = "Order-Success";
+	const ERROR = "Order-Error";
+	
 	public function save()
 	{
 		$sql = new Sql();
@@ -46,6 +50,83 @@ Class Order extends Model{
 			$this->setData($results[0]);
 		}
 	}
+
+	public static function listAll()
+	{
+		$sql = new Sql();
+
+		return $sql->select("
+			SELECT * 
+			FROM tb_orders a 
+			INNER JOIN tb_ordersstatus b USING(idstatus)
+			INNER JOIN tb_carts c USING(idcart)
+			INNER JOIN tb_users d ON d.iduser = a.iduser
+			INNER JOIN tb_addresses e USING(idaddress)
+			INNER JOIN tb_persosns f ON f.idperson = d.idperson
+			ORDER BY a.dtregister DESC
+		");
+	}
+
+	public function delete()
+	{
+		$sql = new Sql();
+
+		$sql->query("DELETE * FROM tb_orders WHERE idorder = :idorder", [
+			':idorder'=>$this->getidorder()
+		]);
+	}
+
+	public function getCart()
+	{
+		$cart = new Cart();
+
+		$cart->get((int)$this->getidcart());
+
+		return $cart;
+	}
+
+	public static function setError($msg)
+	{
+		$_SESSION[Order::ERROR] = $msg;
+	}
+
+	public static function getError()
+	{
+
+		$msg = (isset($_SESSION[Order::ERROR]) && $_SESSION[Order::ERROR]) ? $_SESSION[Order::ERROR] : '';
+
+		Order::clearError();
+
+		return $msg;
+
+	}
+
+	public static function clearError()
+	{
+		$_SESSION[Order::ERROR] = NULL;
+	}
+
+	public static function setSuccess($msg)
+	{
+		$_SESSION[Order::SUCCESS] = $msg;
+	}
+
+	public static function getSuccess()
+	{
+
+		$msg = (isset($_SESSION[Order::SUCCESS]) && $_SESSION[Order::SUCCESS]) ? $_SESSION[Order::SUCCESS] : '';
+
+		Order::clearSuccess();
+
+		return $msg;
+
+	}
+
+	public static function clearSuccess()
+	{
+		$_SESSION[Order::SUCCESS] = NULL;
+	}
+
 }
 
 ?>
