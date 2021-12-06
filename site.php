982c0d34ddb1a484891359193d25769b94d68960
Alan Felipe <alanfelipeop@outlook.com>
2021-12-06 16:07:39 -0300

    Alteração de Senha Usuario

--- site.php
+++ site.php
@@ -571,4 +571,70 @@ $app->get("/profile/orders/:idorder", function($idorder){
 	]);
 });
 
+$app->get("/profile/profile-change-password", function(){
+
+	User::verifyLogin(false);
+
+	$page = new Page();
+
+	$page->setTpl("profile-change-password", [
+		'changePassError'=>User::getError(),
+		'changePassSuccess'=>User::getSuccess()
+	]);
+});
+
+$app->post("/profile/change-password", function(){
+
+	User::verifyLogin(false);
+
+	if(!isset($_POST['current_pass']) || $_POST['current_pass'] === ''){
+		User::setError("Digite a Senha Atual");
+
+		header("Location: /profile/change-password");
+		exit;
+	}
+
+	if(!isset($_POST['new_pass']) || $_POST['new_pass'] === ''){
+		User::setError("Digite a Nova Senha");
+
+		header("Location: /profile/change-password");
+		exit;
+	}
+
+	if(!isset($_POST['new_pass_confirm']) || $_POST['new_pass_confirm'] === ''){
+		User::setError("Confirme sua Nova Senha");
+
+		header("Location: /profile/change-password");
+		exit;
+	}
+
+	if($_POST['current_pass'] === $_POST['new_pass']){
+
+		User::setError("A sua Nova Senha deve ser Diferente da Atual");
+
+		header("Location: /profile/change-password");
+		exit;
+	}
+
+	$user = User::getFromSession();
+
+	if(!password_verify($_POST['current_pass'], $user->getdespassword())){
+
+		User::setError("A Senha está Inavlida");
+
+		header("Location: /profile/change-password");
+		exit;
+	}
+
+	$user->setdespassword($_POST['new_pass']);
+
+	$user->update();
+
+	User::setSuccess("Senha Alterada com Sucesso.");
+
+	header("Location: /profile/change-password");
+	exit;
+
+});
+
 ?>
